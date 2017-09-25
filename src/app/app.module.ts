1b3526eddab2b6dbe1bdf92d56fa90f94b178b82
yiqisweet <mynameislxmax@outlook.com>
2017-09-25 18:19:06 +0800

    insert home component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,16 @@ import { NgModule } from '@angular/core';
 import {FormsModule} from '@angular/forms';
 //引入的组件需要在这里添加
 import { AppComponent } from './app.component';
-import { UserComponent } from './components/user/user.component'
+import { UserComponent } from './components/user/user.component';
+import { HomeComponent } from './components/home/home.component'
 
 @NgModule({
   //自己创建的组件会在这里出现
-  declarations: [
+
+    declarations: [
     AppComponent,
-    UserComponent
+    UserComponent,
+    HomeComponent
   ],
   //模块放在这里，比如说httpModule
   imports: [
